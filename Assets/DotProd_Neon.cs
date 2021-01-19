1ebe7be6dea548bb266156142989e44d1618b3e6
Yury Habets <yury@unity3d.com>
2021-01-19 13:37:21 +0100

    Updated code to use intrinsic APIs instead of C#-isms because it feel more natural. (and produces correct assembly in the end!)
    Updated Unity Editor version to 2021.1.17.

--- Assets/DotProd_Neon.cs
+++ Assets/DotProd_Neon.cs
@@ -1,7 +1,7 @@
 ﻿using Unity.Burst;
 using Unity.Burst.Intrinsics;
 using UnityEngine;
-using static Unity.Burst.Intrinsics.Arm;
+using static Unity.Burst.Intrinsics.Arm.Neon;
 
 public class DotProd_Neon : MonoBehaviour
 {
@@ -41,7 +41,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 4;
                 int iterations = len / elementsPerIteration;
@@ -53,17 +53,17 @@ public class DotProd_Neon : MonoBehaviour
                 for (int i = 0; i < iterations; ++i)
                 {
                     // Load vector elements to registers
-                    var v1 = *(v64*)inputArray1;
-                    var v2 = *(v64*)inputArray2;
+                    var v1 = vld1_s16(inputArray1);
+                    var v2 = vld1_s16(inputArray2);
 
-                    partialSumsNeon = Neon.vmlal_s16(partialSumsNeon, v1, v2);
+                    partialSumsNeon = vmlal_s16(partialSumsNeon, v1, v2);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
@@ -83,7 +83,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon2(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 8;
                 int iterations = len / elementsPerIteration;
@@ -96,22 +96,22 @@ public class DotProd_Neon : MonoBehaviour
                 for (int i = 0; i < iterations; ++i)
                 {
                     // Load vector elements to registers
-                    var v11 = *(v64*)inputArray1;
-                    var v12 = *(v64*)(inputArray1 + 4);
-                    var v21 = *(v64*)inputArray2;
-                    var v22 = *(v64*)(inputArray2 + 4);
+                    var v11 = vld1_s16(inputArray1);
+                    var v12 = vld1_s16(inputArray1 + 4);
+                    var v21 = vld1_s16(inputArray2);
+                    var v22 = vld1_s16(inputArray2 + 4);
 
-                    partialSum1 = Neon.vmlal_s16(partialSum1, v11, v21);
-                    partialSum2 = Neon.vmlal_s16(partialSum2, v12, v22);
+                    partialSum1 = vmlal_s16(partialSum1, v11, v21);
+                    partialSum2 = vmlal_s16(partialSum2, v12, v22);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
                 // Now sum up the results of the 2 partial sums from the loop
-                var partialSumsNeon = Neon.vaddq_s32(partialSum1, partialSum2);
+                var partialSumsNeon = vaddq_s32(partialSum1, partialSum2);
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
@@ -131,7 +131,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon3(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 12;
                 int iterations = len / elementsPerIteration;
@@ -145,26 +145,26 @@ public class DotProd_Neon : MonoBehaviour
                 for (int i = 0; i < iterations; ++i)
                 {
                     // Load vector elements to registers
-                    var v11 = *(v64*)inputArray1;
-                    var v12 = *(v64*)(inputArray1 + 4);
-                    var v13 = *(v64*)(inputArray1 + 8);
-                    var v21 = *(v64*)inputArray2;
-                    var v22 = *(v64*)(inputArray2 + 4);
-                    var v23 = *(v64*)(inputArray2 + 8);
+                    var v11 = vld1_s16(inputArray1);
+                    var v12 = vld1_s16(inputArray1 + 4);
+                    var v13 = vld1_s16(inputArray1 + 8);
+                    var v21 = vld1_s16(inputArray2);
+                    var v22 = vld1_s16(inputArray2 + 4);
+                    var v23 = vld1_s16(inputArray2 + 8);
 
-                    partialSum1 = Neon.vmlal_s16(partialSum1, v11, v21);
-                    partialSum2 = Neon.vmlal_s16(partialSum2, v12, v22);
-                    partialSum3 = Neon.vmlal_s16(partialSum3, v13, v23);
+                    partialSum1 = vmlal_s16(partialSum1, v11, v21);
+                    partialSum2 = vmlal_s16(partialSum2, v12, v22);
+                    partialSum3 = vmlal_s16(partialSum3, v13, v23);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
                 // Now sum up the results of the 3 partial sums from the loop
-                var partialSumsNeon = Neon.vaddq_s32(partialSum1, partialSum2);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum3);
+                var partialSumsNeon = vaddq_s32(partialSum1, partialSum2);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum3);
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
@@ -184,7 +184,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon4(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 16;
                 int iterations = len / elementsPerIteration;
@@ -199,30 +199,30 @@ public class DotProd_Neon : MonoBehaviour
                 for (int i = 0; i < iterations; ++i)
                 {
                     // Load vector elements to registers
-                    var v11 = *(v64*)inputArray1;
-                    var v12 = *(v64*)(inputArray1 + 4);
-                    var v13 = *(v64*)(inputArray1 + 8);
-                    var v14 = *(v64*)(inputArray1 + 12);
-                    var v21 = *(v64*)inputArray2;
-                    var v22 = *(v64*)(inputArray2 + 4);
-                    var v23 = *(v64*)(inputArray2 + 8);
-                    var v24 = *(v64*)(inputArray2 + 12);
+                    var v11 = vld1_s16(inputArray1);
+                    var v12 = vld1_s16(inputArray1 + 4);
+                    var v13 = vld1_s16(inputArray1 + 8);
+                    var v14 = vld1_s16(inputArray1 + 12);
+                    var v21 = vld1_s16(inputArray2);
+                    var v22 = vld1_s16(inputArray2 + 4);
+                    var v23 = vld1_s16(inputArray2 + 8);
+                    var v24 = vld1_s16(inputArray2 + 12);
 
-                    partialSum1 = Neon.vmlal_s16(partialSum1, v11, v21);
-                    partialSum2 = Neon.vmlal_s16(partialSum2, v12, v22);
-                    partialSum3 = Neon.vmlal_s16(partialSum3, v13, v23);
-                    partialSum4 = Neon.vmlal_s16(partialSum4, v14, v24);
+                    partialSum1 = vmlal_s16(partialSum1, v11, v21);
+                    partialSum2 = vmlal_s16(partialSum2, v12, v22);
+                    partialSum3 = vmlal_s16(partialSum3, v13, v23);
+                    partialSum4 = vmlal_s16(partialSum4, v14, v24);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
                 // Now sum up the results of the 3 partial sums from the loop
-                var partialSumsNeon = Neon.vaddq_s32(partialSum1, partialSum2);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum3);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum4);
+                var partialSumsNeon = vaddq_s32(partialSum1, partialSum2);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum3);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum4);
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
@@ -242,7 +242,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon6(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 24;
                 int iterations = len / elementsPerIteration;
@@ -259,38 +259,38 @@ public class DotProd_Neon : MonoBehaviour
                 for (int i = 0; i < iterations; ++i)
                 {
                     // Load vector elements to registers
-                    var v11 = *(v64*)inputArray1;
-                    var v12 = *(v64*)(inputArray1 + 4);
-                    var v13 = *(v64*)(inputArray1 + 8);
-                    var v14 = *(v64*)(inputArray1 + 12);
-                    var v15 = *(v64*)(inputArray1 + 16);
-                    var v16 = *(v64*)(inputArray1 + 20);
-                    var v21 = *(v64*)inputArray2;
-                    var v22 = *(v64*)(inputArray2 + 4);
-                    var v23 = *(v64*)(inputArray2 + 8);
-                    var v24 = *(v64*)(inputArray2 + 12);
-                    var v25 = *(v64*)(inputArray2 + 16);
-                    var v26 = *(v64*)(inputArray2 + 20);
+                    var v11 = vld1_s16(inputArray1);
+                    var v12 = vld1_s16(inputArray1 + 4);
+                    var v13 = vld1_s16(inputArray1 + 8);
+                    var v14 = vld1_s16(inputArray1 + 12);
+                    var v15 = vld1_s16(inputArray1 + 16);
+                    var v16 = vld1_s16(inputArray1 + 20);
+                    var v21 = vld1_s16(inputArray2);
+                    var v22 = vld1_s16(inputArray2 + 4);
+                    var v23 = vld1_s16(inputArray2 + 8);
+                    var v24 = vld1_s16(inputArray2 + 12);
+                    var v25 = vld1_s16(inputArray2 + 16);
+                    var v26 = vld1_s16(inputArray2 + 20);
 
-                    partialSum1 = Neon.vmlal_s16(partialSum1, v11, v21);
-                    partialSum2 = Neon.vmlal_s16(partialSum2, v12, v22);
-                    partialSum3 = Neon.vmlal_s16(partialSum3, v13, v23);
-                    partialSum4 = Neon.vmlal_s16(partialSum4, v14, v24);
-                    partialSum5 = Neon.vmlal_s16(partialSum5, v15, v25);
-                    partialSum6 = Neon.vmlal_s16(partialSum6, v16, v26);
+                    partialSum1 = vmlal_s16(partialSum1, v11, v21);
+                    partialSum2 = vmlal_s16(partialSum2, v12, v22);
+                    partialSum3 = vmlal_s16(partialSum3, v13, v23);
+                    partialSum4 = vmlal_s16(partialSum4, v14, v24);
+                    partialSum5 = vmlal_s16(partialSum5, v15, v25);
+                    partialSum6 = vmlal_s16(partialSum6, v16, v26);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
                 // Now sum up the results of the 6 partial sums from the loop
-                var partialSumsNeon = Neon.vaddq_s32(partialSum1, partialSum2);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum3);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum4);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum5);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum6);
+                var partialSumsNeon = vaddq_s32(partialSum1, partialSum2);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum3);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum4);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum5);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum6);
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
@@ -310,7 +310,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon_with_SMLAL2_2wide(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 8;
                 int iterations = len / elementsPerIteration;
@@ -326,20 +326,21 @@ public class DotProd_Neon : MonoBehaviour
                     // Comparing to SMLAL variant, we're loading 128-bit vectors here (8x short int)
                     // and using SMLAL2 (vmlal_high_s16) to calculate dot product for the upper half
                     // This way, we're doing only half of the loads comparing to dotProductNeon2
-                    var v1 = *(v128*)inputArray1;
-                    var v2 = *(v128*)inputArray2;
+                    var v1 = vld1q_s16(inputArray1);
+                    var v2 = vld1q_s16(inputArray2);
 
-                    partialSumLow = Neon.vmlal_s16(partialSumLow, v1.Lo64, v2.Lo64);
-                    partialSumHigh = Neon.vmlal_high_s16(partialSumHigh, v1, v2);
+                    // Possible to use Lo64 property instead of calling vget_low_s16()
+                    partialSumLow = vmlal_s16(partialSumLow, vget_low_s16(v1), vget_low_s16(v2));
+                    partialSumHigh = vmlal_high_s16(partialSumHigh, v1, v2);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
                 // Now sum up the results of the 2 partial sums from the loop
-                var partialSumsNeon = Neon.vaddq_s32(partialSumLow, partialSumHigh);
+                var partialSumsNeon = vaddq_s32(partialSumLow, partialSumHigh);
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
@@ -359,7 +360,7 @@ public class DotProd_Neon : MonoBehaviour
         [BurstCompile]
         public static unsafe int dotProductNeon_with_SMLAL2_4wide(short* inputArray1, short* inputArray2, short len)
         {
-            if (Neon.IsNeonSupported)
+            if (IsNeonSupported)
             {
                 const int elementsPerIteration = 16;
                 int iterations = len / elementsPerIteration;
@@ -377,26 +378,27 @@ public class DotProd_Neon : MonoBehaviour
                     // Comparing to SMLAL variant, we're loading 128-bit vectors here (8x short int)
                     // and using SMLAL2 (vmlal_high_s16) to calculate dot product for the upper half
                     // This way, we're doing only half of the loads comparing to dotProductNeon2
-                    var v11 = *(v128*)inputArray1;
-                    var v12 = *(v128*)(inputArray1 + 8);
-                    var v21 = *(v128*)inputArray2;
-                    var v22 = *(v128*)(inputArray2 + 8);
+                    var v11 = vld1q_s16(inputArray1);
+                    var v12 = vld1q_s16(inputArray1 + 8);
+                    var v21 = vld1q_s16(inputArray2);
+                    var v22 = vld1q_s16(inputArray2 + 8);
 
-                    partialSum1Low = Neon.vmlal_s16(partialSum1Low, v11.Lo64, v21.Lo64);
-                    partialSum1High = Neon.vmlal_high_s16(partialSum1High, v11, v21);
-                    partialSum2Low = Neon.vmlal_s16(partialSum2Low, v12.Lo64, v22.Lo64);
-                    partialSum2High = Neon.vmlal_high_s16(partialSum2High, v12, v22);
+                    // Possible to use Lo64 property instead of calling vget_low_s16()
+                    partialSum1Low = vmlal_s16(partialSum1Low, vget_low_s16(v11), vget_low_s16(v21));
+                    partialSum1High = vmlal_high_s16(partialSum1High, v11, v21);
+                    partialSum2Low = vmlal_s16(partialSum2Low, vget_low_s16(v12), vget_low_s16(v22));
+                    partialSum2High = vmlal_high_s16(partialSum2High, v12, v22);
 
                     inputArray1 += elementsPerIteration;
                     inputArray2 += elementsPerIteration;
                 }
                 // Now sum up the results of the 2 partial sums from the loop
-                var partialSumsNeon = Neon.vaddq_s32(partialSum1Low, partialSum1High);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum2Low);
-                partialSumsNeon = Neon.vaddq_s32(partialSumsNeon, partialSum2High);
+                var partialSumsNeon = vaddq_s32(partialSum1Low, partialSum1High);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum2Low);
+                partialSumsNeon = vaddq_s32(partialSumsNeon, partialSum2High);
 
                 // Armv8 instruction to sum up all the elements into a single scalar
-                int result = Neon.vaddvq_s32(partialSumsNeon);
+                int result = vaddvq_s32(partialSumsNeon);
 
                 // Calculate the tail
                 int tailLength = len % elementsPerIteration;
