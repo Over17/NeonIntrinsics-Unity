7cb45cf628a0fadfd9b9536d21dcb475b67d6f39
Yury Habets <over17@outlook.com>
2021-01-27 12:41:36 +0100

    Now actually running and displaying the results

--- Assets/DotProd_Neon.cs
+++ Assets/DotProd_Neon.cs
@@ -1,14 +1,145 @@
-﻿using Unity.Burst;
+﻿using System.Diagnostics;
+using System.Text;
+using Unity.Burst;
 using Unity.Burst.Intrinsics;
 using UnityEngine;
+using UnityEngine.UI;
 using static Unity.Burst.Intrinsics.Arm.Neon;
 
 public class DotProd_Neon : MonoBehaviour
 {
+    public Text m_Text;
+
     void Start()
     {
-        
+        // Ramp length and number of trials
+        const int rampLength = 1027;
+        const int trials = 1000000;
+
+        m_Text.text = DoCalc(rampLength, trials);
     }
+
+    private unsafe string DoCalc(short rampLength, int trials)
+    {
+        var sb = new StringBuilder();
+
+        // Generate two input vectors
+        // (0, 1, ..., rampLength - 1)
+        // (100, 101, ..., 100 + rampLength-1)
+        var ramp1 = generateRamp(0, rampLength);
+        var ramp2 = generateRamp(100, rampLength);
+
+        fixed (short* ramp1ptr = ramp1, ramp2ptr = ramp2)
+        {
+            int lastResult = 0;
+            var timer = new Stopwatch();
+
+            sb.AppendLine("----==== NO NEON ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductScalar(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NO NEON, Bursted ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductBurst(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, no unrolling ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 2x unrolling ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon2(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 3x unrolling ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon3(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 4x unrolling ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon4(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, 6x unrolling ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon6(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== NEON, SMLAL+SMLAL2 2-wide ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon_with_SMLAL2_2wide(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+
+            sb.AppendLine("----==== SMLAL+SMLAL2 4-wide ====----");
+            timer.Restart();
+            for (int i = 0; i < trials; i++)
+            {
+                lastResult = CalculateDotProd.dotProductNeon_with_SMLAL2_4wide(ramp1ptr, ramp2ptr, rampLength);
+            }
+            timer.Stop();
+            sb.AppendLine($"Result: {lastResult}");
+            sb.AppendLine($"elapsedMs time: {timer.ElapsedMilliseconds} ms").AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    static short[] generateRamp(short startValue, short len)
+    {
+        var ramp = new short[len];
+
+        for (short i = 0; i < len; i++)
+        {
+            ramp[i] = (short)(startValue + i);
+        }
+
+        return ramp;
+    }
+
 
     [BurstCompile]
     public static class CalculateDotProd
